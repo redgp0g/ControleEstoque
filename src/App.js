0ac521e576188195c13c6f812a5ea280fd364316
Guilherme Gordiano <guilhermeg2004@gmail.com>
2023-07-20 13:57:12 -0300

    Troca do Nome

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <a href="/estoque" className="navbar-brand">
-          bezKoder
+          Controle de Estoque
         </a>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
