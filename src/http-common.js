4f1d48a1b6735dd87bba3d5e22dafd4fc36c546d
Guilherme Gordiano <guilhermeg2004@gmail.com>
2023-07-22 12:59:07 -0300

    URL API

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://estoqueschwarzapi20230720222040.azurewebsites.net/api",
+  baseURL: "estoqueschwazapi.azurewebsites.net",
   headers: {
     "Content-type": "application/json"
   }
